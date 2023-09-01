d6acc667b8d44d75e1e1c23328552610e08a5be7
bau <bau@liis.su>
2023-09-01 09:14:53 +0300

    add info to match player class

--- lib/match/match_player.dart
+++ lib/match/match_player.dart
@@ -1,30 +1,86 @@
 import 'package:rf_core/i_player.dart';
 
 class MatchPlayer implements IPlayer {
+  /// Всего взносов
   static int? matchBallsSum;
-  final int? _pos;
-  final String? _lastName;
-  final String? _firstName;
-  final int? _range;
-  int? _score;
+
+  /// Позиция игрока в турнире
+  final int _pos;
+
+  /// Фамилия игрока
+  final String _lastName;
+
+  /// Имя игрока
+  final String _firstName;
+
+  /// Общий рейтинг игрока в приложении
+  final int _range;
+
+  ///Количество потенциальных матчей в турнире у игрока
+  final int _matchesQnt;
+
+  ///Сумма очков игрока за сыгранные матчи
+  int _score;
+
+  int additionalFee = 0;
+
+  ///Количество очков, которое внес игрок в начале турнира
   late final int? startFee;
+
+  ///Количество очков, которое игрок получил в конце турнира
   late final int? endFee;
+
+  ///Дробная часть от [endFee]
   late final double? endFraction;
+
+
   late final double? matchBallsAsPercent;
-  int? additionalFee = 0;
 
   MatchPlayer({pos, lastName, firstName, range, matchesQnt, score})
       : _pos = pos,
         _lastName = lastName,
         _firstName = firstName,
         _score = score,
+        _matchesQnt = matchesQnt,
         _range = range;
+
+  @override
+  String getFirstName() => _firstName;
+
+  @override
+  String getLastName() => _lastName;
+
+  @override
+  int getRange() => _range;
+
+  int getNewRange() => _range! - startFee! + endFee! + additionalFee!;
+
+  @override
+  int getPos() {
+    return _pos;
+  }
+
+  @override
+  int getMatchesQnt() {
+    return _matchesQnt;
+  }
+
+  @override
+  int getScore() {
+    return _score;
+  }
+
+  @override
+  void setScore(int score) {
+    _score += score;
+  }
 
   MatchPlayer copyWith({
     int? pos,
     String? lastName,
     String? firstName,
     int? range,
+    int? matchesQnt,
     int? score,
   }) {
     return MatchPlayer(
@@ -32,20 +88,18 @@ class MatchPlayer implements IPlayer {
       lastName: lastName ?? _lastName,
       firstName: firstName ?? _firstName,
       range: range ?? _range,
+      matchesQnt: matchesQnt ?? _matchesQnt,
       score: score ?? _score,
     );
   }
 
   @override
-  String? getFirstName() => _firstName;
-
-  @override
-  String? getLastName() => _lastName;
-
-  @override
-  int? getRange() => _range;
-
-  int? getNewRange() => _range! - startFee! + endFee! + additionalFee!;
+  int get hashCode =>
+      _pos.hashCode ^
+      _lastName.hashCode ^
+      _firstName.hashCode ^
+      _range.hashCode ^
+      _score.hashCode;
 
   @override
   bool operator ==(Object other) =>
@@ -59,32 +113,7 @@ class MatchPlayer implements IPlayer {
           _score == other._score;
 
   @override
-  int get hashCode =>
-      _pos.hashCode ^
-      _lastName.hashCode ^
-      _firstName.hashCode ^
-      _range.hashCode ^
-      _score.hashCode;
-
-  @override
-  int? getPos() {
-    return _pos;
-  }
-
-  @override
   String toString() {
-    return 'MatchPlayer{$_pos, $_lastName, $_range}';
-  }
-
-  @override
-  int? getTourneyScore() {
-    return _score;
-  }
-
-  @override
-  void setTourneyScore(int score) {
-    if (_score != null) {
-      _score = _score! + score;
-    }
+    return 'MatchPlayer{_pos: $_pos, _lastName: $_lastName, _firstName: $_firstName, _range: $_range, _matchesQnt: $_matchesQnt, _score: $_score, startFee: $startFee, endFee: $endFee, endFraction: $endFraction, matchBallsAsPercent: $matchBallsAsPercent, additionalFee: $additionalFee}';
   }
 }
--- lib/rf_core.dart
+++ lib/rf_core.dart
@@ -1,3 +1,4 @@
 library rf_core;
 
 import 'judge.dart';
+import 'match/match_player.dart';
